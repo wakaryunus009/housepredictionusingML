7343ab2b5a03e41bc609b0b54cd047c344463049
wakaryunus009 <wakaryunus009@gamil.com>
2023-04-30 13:23:50 +0530

    first commit

---
+++ server/server.py
@@ -0,0 +1,54 @@
+from flask import Flask, request, jsonify
+import util
+
+app = Flask(__name__)
+
+
+# @app.route('/hello')
+# def hello():
+# return "HI"
+#
+@app.route('/get_location_names')
+def get_location_names():
+    response = jsonify({
+        'locations': util.get_location_names()
+    })
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
+
+
+@app.route('/predict_home_price', methods=['POST'])
+def predict_home_price():
+    total_sqft = float(request.form['total_sqft'])
+    location = request.form['location']
+    bhk = int(request.form['bhk'])
+    bath = int(request.form['bath'])
+
+    response = jsonify({
+        'estimated_price': util.get_estimated_price(location, total_sqft, bhk, bath)
+    })
+    response.headers.add('Access-Control-Allow-Origin', '*')
+
+    return response
+
+
+if __name__ == "__main__":
+    print("Starting Python Flask Server For Home Price Prediction...")
+    util.load_saved_artifacts()
+    app.run()
+# from flask import Flask,request, jsonify
+# import util
+# app = Flask(__name__)
+
+# @app.route('/get_location_names')
+# def get_location_names():
+#     response = jsonify({
+#         'locations': util.get_location_names()
+#     })
+#     response.headers.add('Access-Control-Allow-Origin', '*')
+#     return response
+#     return 'Hello World!'
+
+# if __name__ == '__main__':
+#     print("Starting Python Flask Server For Home Price Prediction...")
+#     app.run()
